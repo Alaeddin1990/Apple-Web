3d2300c36d97d86d383a3dc31a5c5f742cb0a786
Alaeddin Shebani <116307373+Alaeddin1990@users.noreply.github.com>
2023-09-09 22:09:41 +0200

    Appel

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+let Cimg = document.querySelector(".Cimg")
+let container = document.querySelector(".container-fluid")
+let imgs = document.querySelector("img")
+
+function phones(iphone) {
+    Cimg.src = iphone
+}
+function colors(bgColor) {
+    container.style.backgroundColor = bgColor
+}
